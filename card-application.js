8f0cccd7f1bf0c102f0469aa8d61b4d963bbd23d
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-06-17 12:31:27 +0300

    Update card-application.js

--- card-application.js
+++ card-application.js
@@ -22,11 +22,19 @@ var app = new Vue({
             width:400,
             height:400
         },
+        text_position:{
+            x:300,
+            y:25
+        },
+        text_properties:{
+            color:'#000'
+        },
+        fill_style:'rgba(255, 0, 0, 0.5)'
     },
     computed:{
         canvas_context:function() {
             return document.getElementById('img-canvas').getContext('2d');
-        }
+        },
     },
     mounted:function() {
         var thisVue = this;
@@ -44,13 +52,23 @@ var app = new Vue({
             anchor.click();
         },
         saveAsPDF:function() {
-            var pdf = new jsPDF();
+            var pdf = new jspdf.jsPDF();
             pdf.addImage(document.getElementById('img-canvas').toDataURL("image/jpg"), 'JPEG', 0, 0);
-            pdf.save("download.pdf");
+            pdf.save("GreetingsCard.pdf");
         },
         drawText:function() {
-            this.canvas_context.drawImage(this.img_obj, 0, 0, this.allowed_dim.width, this.allowed_dim.height);
-            this.canvas_context.fillText(this.text_on_img, 50, 50);
+            var context = this.canvas_context;
+            context.drawImage(this.img_obj, 0, 0, this.allowed_dim.width, this.allowed_dim.height);
+            context.fillStyle = this.text_properties.color;
+            context.fillText(this.text_on_img, this.text_position.x, this.text_position.y);
+            
+            //Add shadow effect
+            context.shadowOffsetX = this.text_position.width + 2;
+            context.shadowOffsetY = this.text_position.height + 22;
+            context.shadowBlur = 2;
+            context.shadowColor = 'rgba(0, 0, 0, 0.5)';
+            
+            context.font = '20px Times New Roman';
         },
         imageSelected:function() {
             if (this.image_file !== null) {
